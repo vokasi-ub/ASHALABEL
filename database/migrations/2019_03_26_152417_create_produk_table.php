d918a5ac883058e624eac38101161783530b406c
unknown <emaliadewantari12.com>
2019-03-27 16:22:02 +0700

    migration and login

---
+++ database/migrations/2019_03_26_152417_create_produk_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->primary('idProduk');
+            $table->string('idProduk',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+}
---
+++ database/migrations/2019_03_26_154151_create_review_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->primary('idReview');
+            $table->string('idReview',30);
+            $table->string('nama');
+            $table->string('email',30);
+            $table->longText('review');
+            $table->string('status',20);
+            $table->timestamp('tanggal')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+}
